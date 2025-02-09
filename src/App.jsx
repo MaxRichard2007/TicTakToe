f36700a92b0d378327c7df2adf34993d02e596da
MaxRichard2007 <max536854@gmail.com>
2025-02-09 18:50:37 +0330

    Feat: Add GameOver component for winner and draw handling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
-import { WINNING_COMBINATTONS } from "./winning-combinations.js";
+import GameOver from "./components/GameOver.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
 const initialGameBoard = [
   [null, null, null],
@@ -35,7 +36,7 @@ function App() {
 
   let winner;
 
-  for (const combination of WINNING_COMBINATTONS) {
+  for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSybol =
       gameBoard[combination[0].row][combination[0].column];
     const secondSquareSybol =
@@ -51,6 +52,8 @@ function App() {
       winner = firstSquareSybol;
     }
   }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -80,7 +83,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
