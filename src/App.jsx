876c60bd8d7a1e26685c01187804f3d700db1cdc
MaxRichard2007 <max536854@gmail.com>
2025-02-09 18:29:47 +0330

    Feat: Add winner determination logic and update GameBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
-import { WINNING_COMBINATTONS } from './winning-combinations.js';
+import { WINNING_COMBINATTONS } from "./winning-combinations.js";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -17,6 +23,34 @@ function deriveActivePlayer(gameTurns) {
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATTONS) {
+    const firstSquareSybol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSybol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSybol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSybol &&
+      firstSquareSybol === secondSquareSybol &&
+      firstSquareSybol === thirdSquareSybol
+    ) {
+      winner = firstSquareSybol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -46,7 +80,8 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>You won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
